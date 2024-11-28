2826a51a11bc5de00ea3d124f6a6bdf7f33310e0
Hellen Tsegelnik <vladarskaya1@gmail.com>
2024-11-27 22:35:06 -0800

    homework3/remark

--- src/main/java/org/prog/Plane.java
+++ src/main/java/org/prog/Plane.java
@@ -1,4 +1,5 @@
 package org.prog;
+
 import org.prog.homework3.HomeWork3;
 
 public class Plane {
@@ -6,28 +7,31 @@ public class Plane {
     public String destination;   // Пункт назначения
     public String airlines;
 
-    public String toString(){
-        return flightId+destination+airlines;
+    public String toString() {
+        return flightId + destination + airlines;
     }
 
-     @Override
-    public int hashCode (){
-         return(flightId+destination+airlines).hashCode();
+    @Override
+    public int hashCode() {
+        return (flightId + destination + airlines).hashCode();
 
     }
+
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Plane) {
             Plane p = (Plane) obj;
-            boolean itsTheSameAirlines = p.airlines.equels(this.airlines) && this.destination.equals(p.destination)
+            boolean itsTheSameAirlines = p.airlines.equals(this.airlines) && this.destination.equals(p.destination)
                     && this.flightId.equals(p.flightId);
 
-            return itsTheSameAirlines;}
-        return false;
+            return itsTheSameAirlines;
         }
-public int getAirlineLength(){
+        return false;
+    }
+
+    public int getAirlineLength() {
         return airlines.length();
-}
+    }
 }
 
 
